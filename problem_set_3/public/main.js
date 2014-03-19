d03cc463d4893701ddf791250511e71982a0ac06
Dana McDonough <danamcd@sbcglobal.net>
2014-03-19 17:24:02 -0500

    code through step 6

--- problem_set_3/public/main.js
+++ problem_set_3/public/main.js
@@ -1,6 +1,7 @@
 var myApp = angular.module('myApp', []);
 
 function mainPageCtrl($scope) {
+	
 	$scope.kitties = [
 		{name: 'KitKat', age: '1', likes_other_cats: false, size: 'small'},
 		{name: 'Purrito', age: '2', likes_other_cats: true, size: 'small'},
@@ -8,4 +9,10 @@ function mainPageCtrl($scope) {
 		{name: 'Fancy', age: '10', likes_other_cats: true, size: 'medium'},
 		{name: 'Boob', age: '5', likes_other_cats: false, size: 'large'}
 	];
+
+	$scope.isAddingCat = false; 
+
+	$scope.saveCat = function () {
+		$scope.isAddingCat = false;
+	};
 }
