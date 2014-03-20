27170d1fa91de26145f819734f6c513475d02de1
Dana McDonough <danamcd@sbcglobal.net>
2014-03-20 18:47:06 -0500

    problem with save button

--- problem_set_3/public/main.js
+++ problem_set_3/public/main.js
@@ -3,11 +3,11 @@ var myApp = angular.module('myApp', []);
 function mainPageCtrl($scope) {
 	
 	$scope.kitties = [
-		{name: 'KitKat', age: '1', likes_other_cats: false, size: 'small'},
-		{name: 'Purrito', age: '2', likes_other_cats: true, size: 'small'},
-		{name: 'Gotcha', age: '6 mo.', likes_other_cats: true, size: 'large'},
-		{name: 'Fancy', age: '10', likes_other_cats: true, size: 'medium'},
-		{name: 'Boob', age: '5', likes_other_cats: false, size: 'large'}
+		{name: 'KitKat', age: 1, likes_other_cats: false, size: 'small'},
+		{name: 'Purrito', age: 2, likes_other_cats: true, size: 'small'},
+		{name: 'Gotcha', age: 1, likes_other_cats: true, size: 'large'},
+		{name: 'Fancy', age: 10, likes_other_cats: true, size: 'medium'},
+		{name: 'Boob', age: 8, likes_other_cats: false, size: 'large'}
 	];
 
 	$scope.isAddingCat = false; 
