2e0eddc61164b872d6938afd6b0d468cb8546d73
Dana McDonough <danamcd@sbcglobal.net>
2014-05-05 17:29:27 -0500

    api.rb

--- problem_set_3/public/main.js
+++ problem_set_3/public/main.js
@@ -2,41 +2,46 @@ var myApp = angular.module('myApp', []);
 
 function mainPageCtrl($scope, $http) {
 
-	//$http.put('/cats/Samson');
-	
-	function getCats () {
-		$http.get('/cats').success(function (data) {
-		$scope.kitties = data.cats;
-		});
-	}
+    //$http.put('/cats/Samson');
 
-	getCats();
+    function getCats() {
+        $http.get('/cats').success(function (data) {
+            $scope.kitties = data.cats;
+        });
+    }
 
-	$scope.addCat = function () {
-		$scope.isAddingCat = true; 
-		$scope.newCat = {name:'', age:'', size:'small'};
-		$scope.formTask = 'new';
-	};
+    getCats();
 
-	$scope.modifyCat = function () {
-		$scope.isModifyingCat = true;
-        $scope.editCat = {name: '', age:'', size:'small'};
-        //not sure how to save old cat name to new cat name
-		$scope.formTask = 'edit';
-		};
+    $scope.addCat = function () {
+        $scope.catFormIsVisible = true;
+        $scope.catFormData = {name: '', age: '', size: 'small'};
+        $scope.formTask = 'new';
+    };
 
-	$scope.saveCat = function () {
-		if ($scope.formTask === 'new') {
-			$scope.isAddingCat = false;
-			$scope.newCat.age = parseInt($scope.newCat.age);
-			$http.post('/cats', {cat: $scope.newCat}).success(function () {
-				alert("New cat was posted successfully!");
-			});
-		} else if($scope.formTask === 'edit') {
-			$scope.isModifyingCatForm = false;
-				alert("Cat was updated successfully!");
-		}
-		getCats();
-	};
-	
+    $scope.modifyCat = function (idx) {
+        console.log("This is cat " + JSON.stringify($scope.kitties[idx]));
+        $scope.catFormIsVisible = true;
+        $scope.catFormData = angular.copy($scope.kitties[idx]);
+        $scope.selectedCat = idx;
+        $scope.formTask = 'edit';
+    };
+
+    $scope.saveCat = function () {
+        switch (formTask) {
+            //change to a switch statement to increase future functionality
+            case 'new':
+                $scope.catFormData.age = parseInt($scope.catFormData.age);
+                $http.post('/cats', {cat: $scope.catFormData}).success(function () {
+                    alert("New cat was posted successfully!");
+                });
+                break;
+            case 'edit':
+                $scope.catFormIsVisible = false;
+                $http.post('/cats', {cat: $scope.catFormData}).success(function () {
+                    alert("Cat was updated successfully!");
+                });
+                break;
+        }
+        getCats();
+    }
 }
